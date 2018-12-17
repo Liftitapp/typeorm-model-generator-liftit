562c07aae6ddb67b611d76c1fdca38a0d81b1df4
Andres Rosero <andres@dre.sh>
2018-12-17 17:59:02 -0500

    delete _id in relationships

--- src/NamingStrategy.ts
+++ src/NamingStrategy.ts
@@ -19,6 +19,18 @@ export class NamingStrategy extends AbstractNamingStrategy {
             columnOldName[0].toLowerCase() +
             columnOldName.substring(1, columnOldName.length);
 
+        if (
+            columnName.toLowerCase().endsWith("id") &&
+            !columnName.toLowerCase().endsWith("guid")
+        ) {
+            columnName = camelCase(
+                columnName.substring(
+                    0,
+                    columnName.toLowerCase().lastIndexOf("id")
+                )
+            );
+        }
+
         if (!isNaN(parseInt(columnName[columnName.length - 1], 10))) {
             columnName = columnName.substring(0, columnName.length - 1);
         }
